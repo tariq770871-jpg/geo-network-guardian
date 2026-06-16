ba69161e74209db651073b6f83bfd93e57c233b8
tariq770871-jpg <tariq770871@gmail.com>
2026-06-16 06:59:50 +0300

    المرحلة 1: كتابة الهياكل الأساسية

---
+++ backend/src/config/app.config.ts
@@ -0,0 +1,4 @@
+export default () => ({
+  port: parseInt(process.env.PORT, 10) || 3000,
+  nodeEnv: process.env.NODE_ENV || 'development',
+});
---
+++ backend/src/config/redis.config.ts
@@ -0,0 +1,6 @@
+export default () => ({
+  redis: {
+    host: process.env.REDIS_HOST || 'localhost',
+    port: parseInt(process.env.REDIS_PORT, 10) || 6379,
+  },
+});
